b3e7a85854487f7d0b63ca294c5a9ea8f2134069
SZTYWNIUTKO123 <151528876+SZTYWNIUTKO123@users.noreply.github.com>
2023-11-23 13:28:20 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const element = document.getElementById("myBtn");
-element.addEventListener("click", randomText);
-
-function randomText(){
-    const funfact = ["wiadomosc1", "wiadomosc2", "wiadomosc3", "wiadomosc4", "wiadomosc5", "wiadomosc6", "wiadomosc7", "wiadomosc8", "wiadomosc9", "wiadomosc10"];
-    let random = Math.floor(Math.random() * funfact.length);
-    let randomElement = funfact[random];
-    document.getElementById("messages").innerHTML = randomElement
-}
