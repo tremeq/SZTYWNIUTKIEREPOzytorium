0ed9718309be48f2d5079c03596f74a56218c8c1
SZTYWNIUTKO123 <151528876+SZTYWNIUTKO123@users.noreply.github.com>
2023-11-24 14:00:23 +0100

    update wiadomo

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function updateClock() { /* Definicja funkcji 'updateClock' do aktualizacji zegara */
-var now = new Date(); /* Utworzenie nowego obiektu daty i czasu */
-var hours = now.getHours(); /* Pobranie aktualnej godziny */
-var minutes = now.getMinutes(); /* Pobranie aktualnych minut */
-var seconds = now.getSeconds(); /* Pobranie aktualnych sekund */
-minutes = minutes < 10 ? '0' + minutes : minutes; /* Dodanie zera przed minutami, jeśli są mniejsze niż 10 */
-seconds = seconds < 10 ? '0' + seconds : seconds; /* Dodanie zera przed sekundami, jeśli są mniejsze niż 10 */
-document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds; /* Aktualizacja treści zegara */
-setTimeout(updateClock, 1000); /* Ponowne wywołanie funkcji co 1000ms (1 sekunda) */
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds;
+    setTimeout(updateClock, 1000);
 }
-updateClock(); /* Pierwsze wywołanie funkcji, aby zainicjować zegar */
+
+updateClock();
+
+document.addEventListener('DOMContentLoaded', function () {
+    const planLekcji = document.getElementById('planLekcji');
+    const zegar = document.getElementById('zegar');
+
+    const dniTygodnia = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek'];
+
+    for (let i = 0; i < 5; i++) {
+        const dzien = document.createElement('div');
+        dzien.className = 'dzien';
+        dzien.innerHTML = `<h2>${dniTygodnia[i]}</h2>`;
+
+        const lekcjeContainer = document.createElement('div');
+        lekcjeContainer.className = 'lekcje-container';
+        lekcjeContainer.style.marginTop = '10px'; // Dodanie marginesu na górze
+        dzien.appendChild(lekcjeContainer);
+
+        // Tworzymy tylko jedną lekcję dla każdego dnia (indeks j = 0)
+        const lekcja = document.createElement('div');
+        lekcja.className = 'lekcja';
+        lekcja.innerHTML = `<span>Lekcja 1</span>`; //<br><span>Godzina rozpoczęcia: 10:00</span>
+        lekcjeContainer.appendChild(lekcja);
+
+        planLekcji.appendChild(dzien);
+    }
+});
